520803fdfe0f4a8b10c348354675e1acf05700d3
DerekSgov <50340998+DerekSgov@users.noreply.github.com>
2019-09-06 13:51:06 -0700

    Populating the contact me page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout, ContainerDiv, ArticleContent, Title } from '../components/common'
+import { Link } from 'gatsby'
 
 const Contact = () => (
     <Layout>
@@ -7,7 +8,10 @@ const Contact = () => (
             <ArticleContent>
                 <section className="post-full-content">
                     <Title>Contact</Title>
-                    <p>Contact me at _____</p>
+                    <h2>Found an issue with the site?</h2>
+                    <p>Make a pull request or create an issue on Github <Link href={`https://github.com/DerekSgov/Portfolio`}>here</Link></p>
+                    <h2>Email?</h2>
+                    <p>Feel free to contact me through my institutional email, dereksiemens@uvic.ca</p>
                 </section>
             </ArticleContent>
         </ContainerDiv>
